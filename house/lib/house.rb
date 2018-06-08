e74fd86927400cc790b08ceadf676d8ab7a9c589
Caroline <mllegeorgesand@gmail.com>
2018-06-08 13:04:21 -0400

    First commit of house built song

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -3,12 +3,16 @@ class House
       1.upto(12).collect {|i| line(i)}.join("\n")
     end
 
+    def fragment
+      "malt that lay in "
+    end
+
     def line(num)
       case num
       when 1
         "This is the house that Jack built.\n"
       when 2
-        "This is the malt that lay in the house that Jack built.\n"
+        "This is the #{fragment} the house that Jack built.\n"
       when 3
         "This is the rat that ate the malt that lay in the house that Jack built.\n"
       when 4
